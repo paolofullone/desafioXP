58ea053f61eb4daac41402f03a08a252b13428e4
paolofullone <paolo.enrico@gmail.com>
2022-07-16 10:39:25 -0300

    feat: :sparkles: clientsController getAll

---
+++ src/controllers/clientsController.js
@@ -0,0 +1,8 @@
+const clientsService = require('../services/clientsServices');
+
+const getAll = async (_req, res) => {
+  const clients = await clientsService.getAll();
+  return res.status(200).json(clients);
+};
+
+export default {getAll};
