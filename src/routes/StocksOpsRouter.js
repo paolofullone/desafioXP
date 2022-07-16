f2ef8cf9e716f360fe83e385ca4b88ba443cc736
paolofullone <paolo.enrico@gmail.com>
2022-07-16 18:13:24 -0300

    feat: :sparkles: stock operations buy and sell with same function

--- src/routes/StocksOpsRouter.js
+++ src/routes/StocksOpsRouter.js
@@ -7,5 +7,6 @@ const validateAuth = require('../middleware/validateAuth');
 
 router.get('/', validateAuth, stocksOpsController.getAll);
 router.post('/purchase', validateAuth, stocksOpsController.create);
+router.post('/sell', validateAuth, stocksOpsController.create);
 
 module.exports = router;
