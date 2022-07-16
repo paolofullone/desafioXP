127035a21cadcfffb2d2b7bc88dbe17a7670cacc
paolofullone <paolo.enrico@gmail.com>
2022-07-16 11:37:17 -0300

    refactor: :recycle: client service reading db ok

---
+++ src/services/clientsServices.js
@@ -0,0 +1,8 @@
+const clientsModel = require('../models/clientsModel');
+
+const getAll = async () => {
+  const clients = await clientsModel.getAll();
+  return clients;
+};
+
+module.exports = {getAll};
