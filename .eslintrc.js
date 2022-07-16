f89e94975fb07a574e6b0697fc4ff88721adf19b
paolofullone <paolo.enrico@gmail.com>
2022-07-16 10:34:48 -0300

    chore: update eslintrc to use google standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
+  'env': {
+    'browser': true,
+    'es2021': true,
   },
-  extends: [
-    'airbnb-base',
+  'extends': [
+    'google',
   ],
-  parserOptions: {
-    ecmaVersion: 'latest',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
   },
-  rules: {
+  'rules': {
   },
 };
