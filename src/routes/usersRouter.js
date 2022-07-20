c61fcb19a272e785eda15e0c708b3f77318bc9e9
paolofullone <paolo.enrico@gmail.com>
2022-07-20 09:11:45 -0300

    feat: :sparkles: validate transaction, identified during swagger documentation

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -6,12 +6,13 @@ const usersController = require('../controllers/usersController');
 const validateAuth = require('../middleware/validateAuth');
 const validateWithdraw = require('../middleware/validateWithdraw');
 const validateNewUser = require('../middleware/validateNewUser');
+const validateTransaction = require('../middleware/ValidateTransaction');
 
 router.use(validateAuth);
 router.get('/', usersController.getAll);
 router.post('/', validateNewUser, usersController.create);
-router.post('/deposit', usersController.transaction);
-router.post('/withdraw', validateWithdraw, usersController.transaction);
+router.post('/deposit', validateTransaction, usersController.transaction);
+router.post('/withdraw', validateTransaction, validateWithdraw, usersController.transaction);
 router.get('/ballance', usersController.getBallance);
 
 module.exports = router;
