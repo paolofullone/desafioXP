0629674891fd0159d8ae31ed6e9ac4e2f5ac6fe6
paolofullone <paolo.enrico@gmail.com>
2022-07-24 13:23:14 -0300

    feat: :sparkles: validate quantity, length and value of new stocks.

--- src/middleware/validateNewStock.js
+++ src/middleware/validateNewStock.js
@@ -6,6 +6,18 @@ const validateNewStock = async (req, res, next) => {
     const error = { status: 400, message: 'Favor informar nome, preço e quantidade corretamente.' };
     throw error;
   }
+  if (availableQuantity < 0) {
+    const error = { status: 400, message: 'Quantidade disponível não pode ser negativa.' };
+    throw error;
+  }
+  if (name.length < 3) {
+    const error = { status: 400, message: 'Nome deve possuir no mínimo 3 caracteres.' };
+    throw error;
+  }
+  if (value < 0) {
+    const error = { status: 400, message: 'Valor da ação não pode ser negativo.' };
+    throw error;
+  }
   next();
 };
 
